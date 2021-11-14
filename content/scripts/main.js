ced9fbffdc526312f24ae4e303db9b7404298fbd
Cambysses <selxxa@gmail.com>
2021-11-14 15:19:34 -0400

    update faq

--- content/scripts/main.js
+++ content/scripts/main.js
@@ -1,6 +1,6 @@
 // Fades out before navigating away.
 function navigateTo(url) {
-    $("html").fadeOut(300, function() {
+    $("html").fadeOut(300, function () {
         window.location = url;
     });
 }
@@ -10,6 +10,13 @@ $().ready(function (e) {
     // Load header and footer.
     $.get("header.html", function (data) {
         $("body").prepend(data);
+
+        // Logo wink on hover.
+        $("#logo").mouseenter(function () {
+            $(this).attr("src", "content/images/logo_winky.webp");
+        }).mouseleave(function () {
+            $(this).attr("src", "content/images/logo.webp");
+        });
     });
     $.get("footer.html", function (data) {
         $("body").append(data);
@@ -17,15 +24,6 @@ $().ready(function (e) {
 
     // Fade site in.
     $("html").fadeIn(300);
-
-
-    // Logo wink on hover.
-    $("#logo").mouseenter(function () {
-        $(this).attr("src", "content/images/logo_winky.webp");
-    })
-        .mouseleave(function () {
-            $(this).attr("src", "content/images/logo.webp");
-        });
 
     // Expand image on click.
     $(".gallery-item").each(function () {
@@ -48,4 +46,5 @@ $().ready(function (e) {
     $(".theater").click(function () {
         $(this).fadeOut();
     });
+
 });
